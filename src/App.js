20cd0607b7fa5909f912a8925e36951f3ac35bba
Pixel-on-Q <jimmygyal10@gmail.com>
2022-08-28 15:24:04 +0530

    modules 20 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, {Component} from "react"; import Person from "./Person/Person";
+// class App extends Component {     state = {         persons: [             {
+//               name: 'Jigmat',                 age: 26             }, {
+//    name: 'Del',                 age: 15             }, {   name: 'Maddy',
+//             age: 26             }         ]     } switchNameHandler = () => {
+//         console.log('button pressed'); this.setState({           persons: [
+//           {                 name: 'Tashi',                 age: 22
+//  }, {                 name: 'Delex',                 age: 5             }, {
+//                name: 'Maddy',                 age: 2             },         ]
+//         });     }  render() {         return (             <div>
+//    <button onClick={this.switchNameHandler}>Switch Names</button> <Person
+// name={this.state.persons[0].name} age={this.state.persons[0].age}>I am from
+// Chuchot</Person>                 <Person name={this.state.persons[1].name}
+// age={this.state.persons[1].age}></Person>               <Person
+// name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+//           </div>         );     } } export default App; import
+// React,{Component} from 'react'; class App extends Component{ render(){
+// return( <h1>Hello</h1>);   } } export default App; /REACT HOOK useSTATE
+import React, {useState} from "react";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = prop => {
+    //useState returns an array of two elements
+    const [PersonState,
+        setPersonState] = useState({
+        persons: [
+            {
+                name: 'RD',
+                age: 10
+            }, {
+                name: 'MD',
+                age: 20
+            }
+        ]
+    });
+
+    const [otherState,
+        setOtherState] = useState('Some other value');
+
+    console.log(PersonState, otherState);
+
+    const switchNameHandler = () => {
+        setPersonState({
+            persons: [
+                {
+                    name: 'DR',
+                    age: 100
+                }, {
+                    name: 'RD',
+                    age: 3000
+                }
+            ]
+        })
+        setOtherState('Some changed value');
+        
+
+    }
+
+    return (
+        <div>
+            <button onClick={switchNameHandler}>Switch</button>
+            <Person name={PersonState.persons[0].name} age={PersonState.persons[0].age}></Person>
+            <Person name={PersonState.persons[1].name} age={PersonState.persons[1].age}></Person>
+        </div>
+
+    );
+
 }
 
 export default App;
